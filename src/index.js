0a4453964fce18088017cb8bed7af7069fcecd1b
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-09 17:15:14 +0000

    Added NavLink component and movie view

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './components/App';
 
 import Home from './views/Home';
+import Movie from './views/Movie';
+
 import './index.css';
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
+      <Route path="/movie" component={Movie} />
     </Route>
   </Router>
 ),  document.getElementById('root'));
