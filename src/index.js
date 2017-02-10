a7444f2b3acdc57a4d32c67e5858d1f7acce4cc2
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-10 16:46:58 +0000

    Fixed the Movies view

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './components/App';
 
 import Home from './views/Home';
-import Movie from './views/Movie';
 
 import './index.css';
 
@@ -13,7 +12,7 @@ ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      <Route path="/movie" component={Movie} />
+      <Route path="/movie" component={Movies} />
     </Route>
   </Router>
 ),  document.getElementById('root'));
