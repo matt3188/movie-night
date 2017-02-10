0d0cf10f09eb4f2b942c0c3931c110980d3e1cfa
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-10 16:41:52 +0000

    Added SearchBar component too Movies view

--- src/views/Movies.js
+++ src/views/Movies.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 
 import {getMovies} from '../utils/api';
 
+import SearchBar from '../components/SearchBar';
 class Movie extends Component {
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      movieSearched: '',
+      movies: []
+    }
+  }
+
+  movieLookUp( term ) {
+    getMovies( term ).then( ( res ) => {
+      this.setState({
+        movies: res.Search
+      });
+    });
+  }
+
+  handleTermChange = ( movieSearched ) => {
+    this.movieLookUp( movieSearched );
+    this.setState({ movieSearched })
+  }
 
   render() {
     return (
       <div className="container">
+        <SearchBar onTermChange={ this.handleTermChange } />
       </div>
     )
   }
