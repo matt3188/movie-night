ff0123ea1283c11c145809c64f2891365652ea1e
Matt Coleman <hello@matt-coleman.co.uk>
2017-02-11 20:09:48 +0000

    Made api call in Home page view ready to display it

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
+import {discoverMovies} from '../utils/api';
 class Home extends Component {
+
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      results: []
+    }
+  }
+
+  componentDidMount() {
+    discoverMovies('2017').then( ( res ) => {
+      this.setState({
+        results: res.results
+      });
+    });
+  }
+
   render() {
     return (
       <div className="app-home">
