040c4b7f07493a914918bca0ee1b85ed124cdacf
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-13 12:07:59 +0000

    Added overview and rating to DiscoverThumb

--- src/components/DiscoverThumb.js
+++ src/components/DiscoverThumb.js
@@ -9,6 +9,7 @@ class DiscoverThumb extends Component {
   }
 
   render() {
+    console.log(this.props)
     const image_url = 'https://image.tmdb.org/t/p/w500'
     return (
       <li className="discover-thumb">
@@ -16,7 +17,9 @@ class DiscoverThumb extends Component {
           <img alt="" src={image_url + this.props.movie.backdrop_path} />
         </div>
         <div className="discover-overview">
+          {!!this.props.movie.vote_average && <div className="average-rating">{this.props.movie.vote_average} ⭑</div>}
           <h2 className="movie-title">{this.props.movie.title}</h2>
+          <p>{this.props.movie.overview}</p>
         </div>
       </li>
     )
