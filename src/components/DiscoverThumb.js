c09332e5d5d757763e88d98ffc51c9c97efadcaa
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-13 12:07:02 +0000

    Changed discover image to the backdrop rather that the poster

--- src/components/DiscoverThumb.js
+++ src/components/DiscoverThumb.js
@@ -9,11 +9,11 @@ class DiscoverThumb extends Component {
   }
 
   render() {
-    const image_url = 'https://image.tmdb.org/t/p/w185'
+    const image_url = 'https://image.tmdb.org/t/p/w500'
     return (
       <li className="discover-thumb">
         <div className="discover-image">
-          <img alt="" src={image_url + this.props.movie.poster_path} />
+          <img alt="" src={image_url + this.props.movie.backdrop_path} />
         </div>
         <div className="discover-overview">
           <h2 className="movie-title">{this.props.movie.title}</h2>
