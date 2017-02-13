e25473a50eefc0577b8f0833f9a64d7c2dc9dd4c
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-13 16:20:20 +0000

    Changed browserHistory to hashHistory
    gh-pages needs something extra to get browserHistory to work, will
    investigate further

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 
 import routes from './routes';
 
 import './index.css';
 
 ReactDOM.render((
-  <Router routes={routes} history={browserHistory} />
+  <Router routes={routes} history={hashHistory} />
 ),  document.getElementById('root'));
