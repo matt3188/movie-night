759d5a7395e93a328a914d41c16829d08d48ad1f
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-15 15:58:41 +0000

    Added year to the API call ,method

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,20 @@
 const tmdb = {
-  api_url: "https://api.themoviedb.org/3",
   api_key: "5cdda7bb71c5c3baa4f26614a0fe0645",
+  api_url: "https://api.themoviedb.org/3",
   image_url: "https://image.tmdb.org/t/p",
 
-  getMovies: function(searchTerm) {
+  getMovies(searchTerm) {
     const searchMovies = `${this.api_url}/search/movie?api_key=${this.api_key}&language=en-UK&query=${searchTerm}&include_adult=false`
 
     return fetch(searchMovies).then((res) => res.json());
   },
 
-  discoverMovies: function(year) {
-    const discover = `${this.api_url}/discover/movie?primary_release_year=${year}&page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-UK&api_key=${this.api_key}`
+  discoverMovies(year, page) {
+    const discover = `${this.api_url}/discover/movie?primary_release_year=${year}&page=${page}&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-UK&api_key=${this.api_key}`
+    console.log(discover)
 
     return fetch(discover).then((res) => res.json());
   }
-
 }
 
 export {tmdb};
