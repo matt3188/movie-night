96cb1f91a83f81575a48b4d67aa0d280cba93bef
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-15 15:57:45 +0000

    Added year to the filter list

--- src/components/FilterSelection.js
+++ src/components/FilterSelection.js
@@ -18,13 +18,22 @@ class FilterSelection extends Component {
   render() {
     return (
       <div className="discover-filters">
-      <label className="most-popular">Most popular of
-        <select className="filter" onChange={ event => this.onInputChange( event.target.value ) }>
-          <option value="2017">2017</option>
-          <option value="2016">2016</option>
-          <option value="2015">2015</option>
-        </select>
-      </label>
+        <form>
+          <label className="most-popular">Most popular of
+            <select className="filter" onChange={ this.onInputChange.bind(this, 'year')} value={this.state.year}>
+              <option value="2017">2017</option>
+              <option value="2016">2016</option>
+              <option value="2015">2015</option>
+            </select>
+          </label>
+          <label className="page-number">Page
+            <select className="filter" onChange={ this.onInputChange.bind(this, 'page')} value={this.state.page}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </label>
+        </form>
       </div>
     )
   }
