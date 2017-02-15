5f707401afa15c3062bd217d198bef5f3023da02
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-02-15 17:43:13 +0000

    Changed the Movie view, so it makes the new API call and sets the results in the state

--- src/views/Movie.js
+++ src/views/Movie.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import {tmdb} from '../utils/api';
 
 class Movie extends Component {
+
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      movie: {}
+    }
+  }
+
+  lookupMovie( id ) {
+    tmdb.lookupMovie( id ).then( ( res ) => {
+      this.setState({
+        movie: res
+      })
+    });
+  }
+
   render() {
     const image_url = tmdb.image_url + '/w500'
 
     return (
       <div className="container">
-      <h1>{this.props.location.state.title}</h1>
-      <p>{this.props.location.state.overview}</p>
-      <img src={image_url + this.props.location.state.backdrop_path} alt={this.props.location.state.title} />
+        <h1>{this.state.movie.title}</h1>
+        <p>{this.state.movie.overview}</p>
+        <img src={image_url + this.state.movie.backdrop_path} alt={this.state.movie.title} />
       </div>
     )
   }
 }
 
 export default Movie;
+
