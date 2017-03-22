176875810e465890a4de7e4fbf2816bcdab73d18
Matt Coleman <Matt_Coleman@dennis.co.uk>
2017-03-22 11:03:42 +0000

    Added test for navigation links

--- src/components/Layout.test.js
+++ src/components/Layout.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './Layout';
+import TestUtils from 'react-addons-test-utils';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Layout />, div);
+});
+
+describe('load container and check links', function() {
+  it('should have 4 nav links', function() {
+    var container = <Layout />;
+    var DOM = TestUtils.renderIntoDocument(container);
+    var links = TestUtils.scryRenderedDOMComponentsWithTag(
+      DOM, 'li');
+    expect(links.length).toEqual(4);
+  });
 });
